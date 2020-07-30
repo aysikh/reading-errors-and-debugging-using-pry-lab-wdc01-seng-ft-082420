5579ddaf1183f1c5eeaa888b89f88b6b7a519e9b
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-07-30 01:57:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -8,3 +8,6 @@ end
 def play_game(x1, x2)
   "#{x1} is better than #{x2}"
 end
+
+# originally the code repeated player1 and player2 but identified them as different names. 
+# for this code to work, we have to change the other player1 and player2 variables 
